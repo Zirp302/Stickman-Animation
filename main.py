44a4e6f501d27090170db6ec5d2a306d4647330a
Zirp302 <vkkakantakt@gmail.com>
2026-01-03 04:03:38 +0500

    Update main.py
    При нажатии на любую кнопку удаления изображение становиться в первоначальную позицию

--- main.py
+++ main.py
@@ -12,6 +12,21 @@ group2 = pyglet.graphics.Group(2)
 group3 = pyglet.graphics.Group(3)
 group4 = pyglet.graphics.Group(4)
 
+# Значения клавишь для их присвоения переменной keys
+keyboard = {
+    key.A: "A",
+    key.B: "B",
+    key.C: "C",
+    key.D: "D",
+    key.E: "E",
+    key.F: "F",
+    key.G: "G",
+    key.H: "H",
+    key.I: "I",
+    key.J: "J",
+    key.K: "K"
+    }
+
 # Координаты всех точек
 xy = {
     "A": {"x": 102, "y": 330},
@@ -27,21 +42,6 @@ xy = {
     "K": {"x": 356, "y": 30}
     }
 
-# Значения кнопок для их присвоения переменной keys
-keyboard = {
-    key.A: "A",
-    key.B: "B",
-    key.C: "C",
-    key.D: "D",
-    key.E: "E",
-    key.F: "F",
-    key.G: "G",
-    key.H: "H",
-    key.I: "I",
-    key.J: "J",
-    key.K: "K"
-    }
-
 # Все точки
 points = {
     'A': sh.Circle(xy['A']['x'], xy['A']['y'], 10, color=(255, 255, 255), batch=pak, group=group3),
@@ -55,21 +55,6 @@ points = {
     'I': sh.Circle(xy['I']['x'], xy['I']['y'], 10, color=(255, 255, 255), batch=pak, group=group3),
     'J': sh.Circle(xy['J']['x'], xy['J']['y'], 10, color=(255, 255, 255), batch=pak, group=group3),
     'K': sh.Circle(xy['K']['x'], xy['K']['y'], 10, color=(255, 255, 255), batch=pak, group=group3)
-}
-
-# Текст для обозначения точек
-texts = {
-    'A': pyglet.text.Label('A', xy['A']['x'] - 5, xy['A']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'B': pyglet.text.Label('B', xy['B']['x'] - 5, xy['B']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'C': pyglet.text.Label('C', xy['C']['x'] - 5, xy['C']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'D': pyglet.text.Label('D', xy['D']['x'] - 5, xy['D']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'E': pyglet.text.Label('E', xy['E']['x'] - 5, xy['E']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'F': pyglet.text.Label('F', xy['F']['x'] - 5, xy['F']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'G': pyglet.text.Label('G', xy['G']['x'] - 5, xy['G']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'H': pyglet.text.Label('H', xy['H']['x'] - 5, xy['H']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'I': pyglet.text.Label('I', xy['I']['x'] - 2, xy['I']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'J': pyglet.text.Label('J', xy['J']['x'] - 5, xy['J']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
-    'K': pyglet.text.Label('K', xy['K']['x'] - 5, xy['K']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4)
     }
 
 # Все линии соеденяющие точки
@@ -84,8 +69,26 @@ line = {
     'HI': sh.Line(xy['H']['x'], xy['H']['y'], xy['I']['x'], xy['I']['y'], 20, color=(133, 133, 133), batch=pak, group=group2),
     'GJ': sh.Line(xy['G']['x'], xy['G']['y'], xy['J']['x'], xy['J']['y'], 20, color=(133, 133, 133), batch=pak, group=group2),
     'JK': sh.Line(xy['J']['x'], xy['J']['y'], xy['K']['x'], xy['K']['y'], 20, color=(133, 133, 133), batch=pak, group=group2)
-}
+    }
 
+# Текст для обозначения точек
+texts = {
+    'A': pyglet.text.Label('A', xy['A']['x'] - 5, xy['A']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'B': pyglet.text.Label('B', xy['B']['x'] - 4, xy['B']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'C': pyglet.text.Label('C', xy['C']['x'] - 4, xy['C']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'D': pyglet.text.Label('D', xy['D']['x'] - 5, xy['D']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'E': pyglet.text.Label('E', xy['E']['x'] - 4, xy['E']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'F': pyglet.text.Label('F', xy['F']['x'] - 3, xy['F']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'G': pyglet.text.Label('G', xy['G']['x'] - 5, xy['G']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'H': pyglet.text.Label('H', xy['H']['x'] - 5, xy['H']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'I': pyglet.text.Label('I', xy['I']['x'] - 2, xy['I']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'J': pyglet.text.Label('J', xy['J']['x'] - 2, xy['J']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4),
+    'K': pyglet.text.Label('K', xy['K']['x'] - 4, xy['K']['y'] - 5, color=(0, 0, 0), batch=pak, group=group4)
+    }
+
+xy_del = xy.copy()
+for i in xy:
+    xy_del[i] = xy[i].copy()
 
 
 # Круг для создания головы и чёрной дырки в ней
@@ -97,17 +100,71 @@ ugl_head_pi = 157080
 # Ключ для texts и points
 keys = 'A'
 # Показатель передвигаемой точки
-text_key = pyglet.text.Label('A', 20, 540, font_size=40, color=(255, 255, 255), batch=pak)
-
+text_key = pyglet.text.Label('A', 20, 540, font_size=40, color=(255, 255, 255), batch=pak, group=group4)
+head_size = False
+head_thickness = False
 
 @wind.event
 def on_key_press(symbol, modifiers):
-    global keys, keyboard
+    global keys, keyboard, head_size, head_thickness, ugl_head_pi
+    global xy, texts, points, line, xy_del
 
     # Переопределение ключа под одну из передвигаемых точек
     if symbol in keyboard:
         keys = keyboard[symbol]
         text_key.text = keys
+    # Проверка нажатия на Shift
+    elif symbol == key.LSHIFT or symbol == key.RSHIFT:
+        head_size = True
+    # Проверка нажатия на Ctrl
+    elif symbol == key.LCTRL or symbol == key.RCTRL:
+        head_thickness = True
+    # Проверка нажатия на клавиши удаления
+    elif symbol == key.DELETE or symbol == key.BACKSPACE:
+        # Сбрасывет все координыаты до изначальных
+        for i in xy_del:
+            xy[i] = xy_del[i].copy()
+        
+        # Возвращает все линии на изначальные позиции
+        for key_line in line:
+            line[key_line].x = xy[key_line[0]]['x']
+            line[key_line].y = xy[key_line[0]]['y']
+            line[key_line].x2 = xy[key_line[1]]['x']
+            line[key_line].y2 = xy[key_line[1]]['y']
+        
+        # Возвращает все точки и текст на изначальные позиции
+        for i in points:
+            points[i].x = xy[i]['x']
+            points[i].y = xy[i]['y']
+
+            texts[i].x = xy[i]['x'] - texts[i].content_width // 2
+            texts[i].y = xy[i]['y'] - 5
+            print(i, texts[i].content_width // 2)
+        
+        head.radius = 80
+        head_hole.radius = 60
+        ugl_head_pi = 157080
+
+        head.x = xy['E']['x']
+        head.y = xy['E']['y'] + head.radius
+
+        head_hole.x = head.x
+        head_hole.y = head.y
+        
+
+            
+
+
+@wind.event
+def on_key_release(symbol, modifiers):
+    global head_size, head_thickness
+
+    # Проверка отжатия Shift
+    if symbol == key.LSHIFT or symbol == key.RSHIFT:
+        head_size = False
+    # Проверка отжатия Ctrl
+    elif symbol == key.LCTRL or symbol == key.RCTRL:
+        head_thickness = False
 
 
 @wind.event
@@ -120,7 +177,7 @@ def on_mouse_press(x, y, button, modifiers):
     points[keys].x = x
     points[keys].y = y
 
-    texts[keys].x = x - (texts[keys].content_width // 2)
+    texts[keys].x = x - texts[keys].content_width // 2
     texts[keys].y = y - 5
 
     for i in line:
@@ -144,15 +201,28 @@ def on_mouse_press(x, y, button, modifiers):
 @wind.event
 def on_mouse_scroll(x, y, scroll_x, scroll_y):
     global xy, head, head_hole, ugl_head_pi
-    
-    ugl_head_pi += 31416 * scroll_y
-    if ugl_head_pi > 628318 or ugl_head_pi < -628318:
-        ugl_head_pi = 0
 
+    if head_size:
+        if head.radius + scroll_y >= 0:
+            head.radius += scroll_y
+            head_hole.radius += scroll_y
+    
+    elif head_thickness:
+        if head.radius + 1 >= head_hole.radius - scroll_y >= 0:
+            head_hole.radius -= scroll_y
+    
+    else:
+        ugl_head_pi += 31416 * scroll_y
+        if ugl_head_pi > 628318 or ugl_head_pi < -628318:
+            ugl_head_pi = 0
+    
     head.x = round(xy['E']['x'] + head.radius * cos(ugl_head_pi / 100000), 3)
     head.y = round(xy['E']['y'] + head.radius * sin(ugl_head_pi / 100000), 3)
+    
     head_hole.x = head.x
     head_hole.y = head.y 
+
+
 
 # Отрисовка 
 @wind.event
